e4ed2ccbf7d0a81309f31b32d8710140ecf0b44f
Frandy_DLC <82332172+anfoxy-ware@users.noreply.github.com>
2026-06-06 21:14:37 -1200

    prueba2

--- server.js
+++ server.js
@@ -356,7 +356,7 @@ cron.schedule('30 13 * * *', () => {
 }, { timezone: "America/Santo_Domingo" });
 
 // CRON 3: 19:30 PM (7:30 PM) hora RD
-cron.schedule('14 21 * * *', () => {
+cron.schedule('17 21 * * *', () => {
   procesarRecordatoriosPorHora('reminder_time_1930', '19:30 PM');
 }, { timezone: "America/Santo_Domingo" });
 
